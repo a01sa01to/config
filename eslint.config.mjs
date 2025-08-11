6c0ba557391d3f460ddcf5bde2d42769560e9a83
Asa <26807394+a01sa01to@users.noreply.github.com>
2025-08-11 13:59:38 +0900

    fix eslint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,3 +1,6 @@
 import myConfig from '@a01sa01to/eslint-config'
 
-export default [...myConfig, { ignores: ['.yarn/*', '.pnp*', '**/dist/*'] }]
+export default [
+  ...myConfig.default,
+  { ignores: ['.yarn/*', '.pnp*', '**/dist/*'] },
+]
