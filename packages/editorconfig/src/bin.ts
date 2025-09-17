821106eeff27c419a2d7e0331bec26955acae271
Asa <26807394+a01sa01to@users.noreply.github.com>
2025-09-17 20:15:09 +0900

    feat: impl editorconfig package

--- packages/editorconfig/src/bin.ts
+++ packages/editorconfig/src/bin.ts
@@ -1,3 +1,42 @@
 #!/usr/bin/env node
 
-console.log('Hello World!')
+import { argv } from 'node:process'
+import { copyFile } from 'node:fs/promises'
+import { fileURLToPath } from 'node:url'
+import { resolve } from 'node:path'
+
+import chalk from 'chalk'
+import yargs from 'yargs'
+
+const logger = (debug: boolean) => ({
+  debug: (msg: string) => {
+    if (debug) console.debug(`${chalk.bgGray.black(' DEBUG ')} ${msg}`)
+  },
+  info: (msg: string) => {
+    console.info(`${chalk.bgCyan.black(' INFO ')} ${msg}`)
+  },
+})
+
+const main = async () => {
+  const options = await yargs(argv)
+    // eslint-disable-next-line sort-keys
+    .option('debug', { type: 'boolean', default: false })
+    .parse()
+  const { debug, info } = logger(options.debug)
+
+  debug('Debug mode is on')
+
+  const editorConfigFilepath = fileURLToPath(
+    import.meta.resolve('../.editorconfig')
+  )
+  debug(`Resolved .editorconfig path: ${editorConfigFilepath}`)
+
+  debug(`Current working directory: ${process.cwd()}`)
+
+  const savePath = resolve(process.cwd(), '.editorconfig')
+  info(`Copying .editorconfig to ${savePath}`)
+  await copyFile(editorConfigFilepath, savePath)
+  info('Done!')
+}
+
+void main()
