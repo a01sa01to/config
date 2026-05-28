71fb88b4ed2502bc53fe642326336887235bc438
Asa <26807394+a01sa01to@users.noreply.github.com>
2026-05-28 23:50:26 +0900

    chore: upgrade eslint

--- packages/eslint/test/common.ts
+++ packages/eslint/test/common.ts
@@ -4,8 +4,7 @@ import config from '../src/index.js'
 
 const createESLintInstance = () =>
   new ESLint({
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any, @typescript-eslint/no-unsafe-assignment
-    overrideConfig: config as any,
+    overrideConfig: config,
     overrideConfigFile: true,
   })
 
